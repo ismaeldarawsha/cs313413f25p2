2a00ef500d7c2af91e2b01a3eaa5f4008ce7c150
ismaeldarawsha <120052767+ismaeldarawsha@users.noreply.github.com>
2025-09-24 14:52:19 -0500

    Update TestIterator.java

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -3,10 +3,10 @@ package cs271.lab.list;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.After;
 import org.junit.Before;
@@ -15,14 +15,12 @@ import org.junit.Test;
 public class TestIterator {
 
   private List<Integer> list;
-  // See the Java List Interface documentation to understand what all the List methods do ...
 
   @Before
   public void setUp() throws Exception {
+    // Use ArrayList for testing; you can switch to LinkedList to see behavior differences
     list = new ArrayList<Integer>();
-    // TODO Question: Also try with a LinkedList - does it make any difference? 
-    // Answer: No behavioral difference in the sense that both wrok the same for correctness on tests, the only difference
-    // is in performance between Array List and LinkedList
+    // list = new LinkedList<Integer>(); // optional
   }
 
   @After
@@ -45,65 +43,66 @@ public class TestIterator {
     list.add(55);
     list.add(77);
     list.add(66);
+
     final var i = list.iterator();
     assertTrue(i.hasNext());
     assertEquals(33, i.next().intValue());
-  // TODO fix the expected values in the assertions below
-assertTrue(i.hasNext());
-assertEquals(33, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(77, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(44, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(77, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(55, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(77, i.next().intValue());
-assertTrue(i.hasNext());
-assertEquals(66, i.next().intValue());
-assertFalse(i.hasNext());
-}
+    assertTrue(i.hasNext());
+    assertEquals(77, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(44, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(77, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(55, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(77, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(66, i.next().intValue());
+    assertFalse(i.hasNext());
+  }
 
-@Test
-public void testRemove() {
-  list.add(33);
-  list.add(77);
-  list.add(44);
-  list.add(77);
-  list.add(55);
-  list.add(77);
-  list.add(66);
-  final var i = list.iterator();
-  while (i.hasNext()) {
-    if (i.next() == 77) {
-      i.remove(); 
-      // ✅ Question Answer: If you use list.remove(Integer.valueOf(77)) here
-      // instead of i.remove(), it will throw a ConcurrentModificationException,
-      // because you’re directly modifying the list while iterating.
+  @Test
+  public void testRemove() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    final var i = list.iterator();
+    while (i.hasNext()) {
+      if (i.next() == 77) {
+        i.remove(); // safe removal during iteration
+      }
     }
+
+    // After removal, the list should contain only: 33, 44, 55, 66
+    assertEquals(List.of(33, 44, 55, 66), list);
   }
-  // ✅ After removing all 77s, the list contains [33, 44, 55, 66]
-  assertEquals(List.of(33, 44, 55, 66), list);
+
+  @Test
+  public void testAverageValues() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    double sum = 0;
+    int n = 0;
+    final Iterator<Integer> i = list.iterator();
+    while (i.hasNext()) {
+      sum += i.next();
+      n++;
+    }
+
+    assertEquals(61.3, sum / n, 0.1); // average
+    assertEquals(7, n);               // count
+  }
 }
 
-@Test
-public void testAverageValues() {
-  list.add(33);
-  list.add(77);
-  list.add(44);
-  list.add(77);
-  list.add(55);
-  list.add(77);
-  list.add(66);
-  double sum = 0;
-  int n = 0;
-  final var i = list.iterator();
-  while (i.hasNext()) {
-    sum += i.next();
-    n++;
-  }
-  assertEquals(61.3, sum / n, 0.1);
-  assertEquals(7, n);
-}

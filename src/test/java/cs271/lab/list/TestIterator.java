815231ab0a593df8e73ab1c4c8f1d5d6876b711e
ismaeldarawsha <120052767+ismaeldarawsha@users.noreply.github.com>
2025-09-24 14:45:34 -0500

    Update TestIterator.java

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -20,7 +20,9 @@ public class TestIterator {
   @Before
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
-    // TODO Question: Also try with a LinkedList - does it make any difference?
+    // TODO Question: Also try with a LinkedList - does it make any difference? 
+    // Answer: No behavioral difference in the sense that both wrok the same for correctness on tests, the only difference
+    // is in performance between Array List and LinkedList
   }
 
   @After
@@ -46,58 +48,62 @@ public class TestIterator {
     final var i = list.iterator();
     assertTrue(i.hasNext());
     assertEquals(33, i.next().intValue());
-    // TODO fix the expected values in the assertions below
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
-    assertFalse(i.hasNext());
-  }
+  // TODO fix the expected values in the assertions below
+assertTrue(i.hasNext());
+assertEquals(33, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(77, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(44, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(77, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(55, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(77, i.next().intValue());
+assertTrue(i.hasNext());
+assertEquals(66, i.next().intValue());
+assertFalse(i.hasNext());
+}
 
-  @Test
-  public void testRemove() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    final var i = list.iterator();
-    while (i.hasNext()) {
-      if (i.next() == 77) {
-        i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
-      }
+@Test
+public void testRemove() {
+  list.add(33);
+  list.add(77);
+  list.add(44);
+  list.add(77);
+  list.add(55);
+  list.add(77);
+  list.add(66);
+  final var i = list.iterator();
+  while (i.hasNext()) {
+    if (i.next() == 77) {
+      i.remove(); 
+      // ✅ Question Answer: If you use list.remove(Integer.valueOf(77)) here
+      // instead of i.remove(), it will throw a ConcurrentModificationException,
+      // because you’re directly modifying the list while iterating.
     }
-    // TODO using assertEquals and List.of, express which values are left in the list
-    // See TestList.java for examples of how to use List.of; also see the Java List
-    // interface for more information
-    fail("Not yet implemented"); // remove this line when done
   }
+  // ✅ After removing all 77s, the list contains [33, 44, 55, 66]
+  assertEquals(List.of(33, 44, 55, 66), list);
+}
 
-  @Test
-  public void testAverageValues() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    double sum = 0;
-    int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
-    // (defined as the sum of the items divided by the number of items)
-    // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
-    assertEquals(61.3, sum / n, 0.1);
-    assertEquals(7, n);
+@Test
+public void testAverageValues() {
+  list.add(33);
+  list.add(77);
+  list.add(44);
+  list.add(77);
+  list.add(55);
+  list.add(77);
+  list.add(66);
+  double sum = 0;
+  int n = 0;
+  final var i = list.iterator();
+  while (i.hasNext()) {
+    sum += i.next();
+    n++;
   }
+  assertEquals(61.3, sum / n, 0.1);
+  assertEquals(7, n);
 }
